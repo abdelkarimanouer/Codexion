064fe4c90885d271bc5a9921726b75c24d0534d5
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-03-27 16:00:32 +0100

    update file

--- coders/main.c
+++ coders/main.c
@@ -1,17 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/27 15:56:20 by aanouer           #+#    #+#             */
+/*   Updated: 2026/03/27 15:57:31 by aanouer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    if (argc == 9)
-    {
-        
-    }
-    else
-    {
-        write(2, "Invalid Args\n", 13);
-        return (1);
-    }
-
-    return (0);
+	if (argc == 9)
+	{
+	}
+	else
+	{
+		write(2, "Invalid Args\n", 13);
+		return (1);
+	}
+	return (0);
 }
