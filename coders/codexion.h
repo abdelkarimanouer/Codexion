e486fa6907a2461bcfeffc31d60eb5ddee51bdf5
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-03-30 16:26:39 +0100

    update file

--- coders/codexion.h
+++ coders/codexion.h
@@ -6,24 +6,57 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 15:50:49 by aanouer           #+#    #+#             */
-/*   Updated: 2026/03/27 16:11:46 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/03/30 16:23:24 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CODEXION_H
 # define CODEXION_H
 
-typedef struct s_codexion
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <string.h>
+
+typedef struct s_dongle
 {
-	long	number_of_coders;
-	long	time_to_burnout;
-	long	time_to_compile;
-	long	time_to_debug;
-	long	time_to_refactor;
-	long	number_of_compiles_required;
-	long	dongle_cooldown;
-	char	*scheduler;
-	int		stop_simulation;
-}	t_codexion;
+	pthread_mutex_t	mutex;
+	long long		cooldown_until;
+	int				is_available;
+	pthread_cond_t	condition;
+}	t_dongle;
+
+typedef struct s_coder
+{
+	int					id;
+	pthread_t			thread;
+	struct s_dongle		*left_dongle;
+	struct s_dongle		*right_dongle;
+	long long			last_compile_time;
+	int					compile_count;
+	long long			deadline;
+	struct s_simulation	*sim;
+}	t_coder;
+
+typedef struct s_simulation
+{
+	long			number_of_coders;
+	long			time_to_burnout;
+	long			time_to_compile;
+	long			time_to_debug;
+	long			time_to_refactor;
+	long			number_of_compiles_required;
+	long			dongle_cooldown;
+	char			*scheduler;
+	long long		start_time;
+	int				stop_simulation;
+	t_coder			*coders;
+	t_dongle		*dongles;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	stop_mutex;
+	pthread_t		monitor_thread;
+}	t_simulation;
 
 #endif
