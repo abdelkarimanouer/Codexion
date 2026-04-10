0a4e9a83b7ffd661b15bd3cbcf2c8e8659b83fa4
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-04-10 21:58:16 +0100

    update file

--- coders/scheduler.c
+++ coders/scheduler.c
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/10 20:59:35 by aanouer           #+#    #+#             */
-/*   Updated: 2026/04/10 21:50:38 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/04/10 21:57:08 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	add_coder_to_queue(t_dongle *dongle, t_coder *coder, char *scheduler)
 {
 	int	i;
+	int	j;
 
 	if (strcmp(scheduler, "fifo") == 0)
 	{
@@ -24,13 +25,17 @@ void	add_coder_to_queue(t_dongle *dongle, t_coder *coder, char *scheduler)
 	else if (strcmp(scheduler, "edf") == 0)
 	{
 		i = 0;
-		while (i < dongle->waiting_count)
-		{
-			if (dongle->waiting_queue[i]->deadline < coder->deadline)
-				break;
+		while (i < dongle->waiting_count && 
+			dongle->waiting_queue[i]->deadline < coder->deadline)
 			i++;
+		j = dongle->waiting_count;
+		while (j > i)
+		{
+			dongle->waiting_queue[j] = dongle->waiting_queue[j - 1];
+			j--;
 		}
-		
+		dongle->waiting_queue[i] = coder;
+		dongle->waiting_count++;
 	}
 	else
 		return ;
