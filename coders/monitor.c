205b68554ad072ce6c596f731f10fe6fa696e288
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-04-12 14:17:04 +0100

    update file

--- coders/monitor.c
+++ coders/monitor.c
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/10 11:32:03 by aanouer           #+#    #+#             */
-/*   Updated: 2026/04/12 14:11:51 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/04/12 14:16:38 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static void	check_if_coders_compiled_enough(t_simulation *sim, int *i)
 		sim->stop_simulation = 1;
 }
 
-static long long	get_time()
+static long long	get_time(void)
 {
 	struct timeval	tv;
 
