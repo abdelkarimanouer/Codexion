22cac2546fe99e5dd32b1d08d8244acc4a63b04d
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-04-12 14:44:51 +0100

    update file

--- coders/coder_routine.c
+++ coders/coder_routine.c
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/05 10:59:36 by aanouer           #+#    #+#             */
-/*   Updated: 2026/04/10 10:54:42 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/04/12 14:35:28 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	*coder_routine(void *arg)
 {
 	t_coder			*coder;
 	struct timeval	tv;
+	long long		current_time;
 
 	coder = (t_coder *)arg;
 	while (coder->sim->stop_simulation == 0)
@@ -24,9 +25,11 @@ void	*coder_routine(void *arg)
 		take_right_dongle(coder);
 		log_action(coder->sim, coder->id, "is compiling");
 		gettimeofday(&tv, NULL);
-		coder->last_compile_time = ((long long)tv.tv_sec * 1000000LL
-				+ tv.tv_usec);
+		current_time = (long long)tv.tv_sec * 1000000LL + tv.tv_usec;
+		pthread_mutex_lock(&coder->sim->stop_mutex);
+		coder->last_compile_time = current_time;
 		coder->compile_count += 1;
+		pthread_mutex_unlock(&coder->sim->stop_mutex);
 		usleep(coder->sim->time_to_compile * 1000);
 		release_left_dongle(coder);
 		release_right_dongle(coder);
--- coders/monitor.c
+++ coders/monitor.c
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/10 11:32:03 by aanouer           #+#    #+#             */
-/*   Updated: 2026/04/12 14:16:38 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/04/12 14:41:45 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,22 @@ static void	check_if_coders_compiled_enough(t_simulation *sim, int *i)
 {
 	while (*i < sim->number_of_coders)
 	{
-		if (sim->coders[*i].compile_count < sim->number_of_compiles_required)
+		pthread_mutex_lock(&sim->stop_mutex);
+		if (sim->coders[*i].compile_count
+			< sim->number_of_compiles_required)
+		{
+			pthread_mutex_unlock(&sim->stop_mutex);
 			break ;
+		}
+		pthread_mutex_unlock(&sim->stop_mutex);
 		(*i)++;
 	}
 	if (*i == sim->number_of_coders)
+	{
+		pthread_mutex_lock(&sim->stop_mutex);
 		sim->stop_simulation = 1;
+		pthread_mutex_unlock(&sim->stop_mutex);
+	}
 }
 
 static long long	get_time(void)
@@ -32,31 +42,49 @@ static long long	get_time(void)
 	return ((long long)tv.tv_sec * 1000000LL + tv.tv_usec);
 }
 
+static void	check_burnout(t_simulation *sim, int *i)
+{
+	long long	current_time;
+
+	while (*i < sim->number_of_coders)
+	{
+		current_time = get_time();
+		pthread_mutex_lock(&sim->stop_mutex);
+		sim->t_s_comp = current_time - sim->coders[*i].last_compile_time;
+		pthread_mutex_unlock(&sim->stop_mutex);
+		if (sim->t_s_comp > (sim->time_to_burnout * 1000))
+		{
+			log_action(sim, sim->coders[*i].id, "burned out");
+			pthread_mutex_lock(&sim->stop_mutex);
+			sim->stop_simulation = 1;
+			pthread_mutex_unlock(&sim->stop_mutex);
+			break ;
+		}
+		(*i)++;
+	}
+}
+
 void	*monitor_routine(void *arg)
 {
 	t_simulation	*sim;
 	int				i;
-	long long		current_time;
+	int				should_continue;
 
 	sim = (t_simulation *)arg;
+	pthread_mutex_lock(&coder->sim->stop_mutex);
+	should_continue = (coder->sim->stop_simulation == 0);
+	pthread_mutex_unlock(&coder->sim->stop_mutex);
+	pthread_mutex_lock(&sim->stop_mutex);
 	while (sim->stop_simulation == 0)
 	{
+		pthread_mutex_unlock(&sim->stop_mutex);
 		i = 0;
-		while (i < sim->number_of_coders)
-		{
-			current_time = get_time();
-			sim->t_s_comp = current_time - sim->coders[i].last_compile_time;
-			if (sim->t_s_comp > (sim->time_to_burnout * 1000))
-			{
-				log_action(sim, sim->coders[i].id, "burned out");
-				sim->stop_simulation = 1;
-				break ;
-			}
-			i++;
-		}
+		check_burnout(sim, &i);
 		i = 0;
 		check_if_coders_compiled_enough(sim, &i);
 		usleep(1000);
+		pthread_mutex_lock(&sim->stop_mutex);
 	}
+	pthread_mutex_unlock(&sim->stop_mutex);
 	return (NULL);
 }
