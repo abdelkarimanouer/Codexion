576afbc7c82775831469f0dfd06c7b70d0a24c95
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-04-13 14:01:10 +0100

    update file

--- coders/coder_routine.c
+++ coders/coder_routine.c
@@ -6,18 +6,32 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/05 10:59:36 by aanouer           #+#    #+#             */
-/*   Updated: 2026/04/12 15:00:59 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/04/13 13:54:13 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
+static int	is_stopped(t_simulation *sim)
+{
+	int	stopped;
+
+	pthread_mutex_lock(&sim->stop_mutex);
+	stopped = sim->stop_simulation;
+	pthread_mutex_unlock(&sim->stop_mutex);
+	return (stopped);
+}
+
 static void	rest_of_code(t_coder *coder)
 {
 	release_left_dongle(coder);
 	release_right_dongle(coder);
+	if (is_stopped(coder->sim))
+		return ;
 	log_action(coder->sim, coder->id, "is debugging");
 	usleep(coder->sim->time_to_debug * 1000);
+	if (is_stopped(coder->sim))
+		return ;
 	log_action(coder->sim, coder->id, "is refactoring");
 	usleep(coder->sim->time_to_refactor * 1000);
 }
@@ -27,23 +41,25 @@ void	*coder_routine(void *arg)
 	t_coder			*coder;
 	struct timeval	tv;
 	long long		current_time;
-	int				should_continue;
 
 	coder = (t_coder *)arg;
-	pthread_mutex_lock(&coder->sim->stop_mutex);
-	should_continue = (coder->sim->stop_simulation == 0);
-	pthread_mutex_unlock(&coder->sim->stop_mutex);
-	while (should_continue)
+	while (!is_stopped(coder->sim))
 	{
 		take_left_dongle(coder);
+		if (is_stopped(coder->sim))
+			return (NULL);
 		take_right_dongle(coder);
+		if (is_stopped(coder->sim))
+		{
+			release_left_dongle(coder);
+			return (NULL);
+		}
 		log_action(coder->sim, coder->id, "is compiling");
 		gettimeofday(&tv, NULL);
 		current_time = (long long)tv.tv_sec * 1000000LL + tv.tv_usec;
 		pthread_mutex_lock(&coder->sim->stop_mutex);
 		coder->last_compile_time = current_time;
 		coder->compile_count += 1;
-		should_continue = (coder->sim->stop_simulation == 0);
 		pthread_mutex_unlock(&coder->sim->stop_mutex);
 		usleep(coder->sim->time_to_compile * 1000);
 		rest_of_code(coder);
--- coders/logging.c
+++ coders/logging.c
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/10 11:18:32 by aanouer           #+#    #+#             */
-/*   Updated: 2026/04/10 11:24:34 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/04/13 13:52:32 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,13 @@ void	log_action(t_simulation *sim, int coder_id, char *log)
 	struct timeval	tv;
 	long long		current_time;
 	long long		timestamp;
+	int				stopped;
 
+	pthread_mutex_lock(&sim->stop_mutex);
+	stopped = sim->stop_simulation;
+	pthread_mutex_unlock(&sim->stop_mutex);
+	if (stopped && strcmp(log, "burned out") != 0)
+		return ;
 	gettimeofday(&tv, NULL);
 	current_time = (long long)tv.tv_sec * 1000000LL + tv.tv_usec;
 	timestamp = (current_time - sim->start_time) / 1000;
