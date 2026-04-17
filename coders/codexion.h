54a641ef2354b970bc9ae08ae8345b75d827f380
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-04-17 16:44:57 +0100

    create t_queue struct

--- coders/codexion.h
+++ coders/codexion.h
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 15:50:49 by aanouer           #+#    #+#             */
-/*   Updated: 2026/04/17 16:41:09 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/04/17 16:44:52 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,8 @@ typedef struct s_request
 typedef struct s_queue
 {
 	struct s_request	**requests;
-	int					number_of_tickets;
-	int					*max_nums_of_tickets;
+	long				number_of_tickets;
+	long				max_nums_of_tickets;
 }			t_queue;
 
 typedef struct s_simulation
