bd598398cc614ab2d2ce2f7574eee08926225a6b
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-04-17 15:21:34 +0100

    update file

--- coders/parsing.c
+++ coders/parsing.c
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 07:51:36 by aanouer           #+#    #+#             */
-/*   Updated: 2026/04/17 15:18:53 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/04/17 15:20:59 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,17 +42,17 @@ int	parsing_args(t_simulation *sim, char **v)
 {
 	if (is_num(v[1]))
 		sim->number_of_coders = atoi(v[1]);
-	if (is_num(v[1]))
+	if (is_num(v[2]))
 		sim->time_to_burnout = atoi(v[2]);
-	if (is_num(v[1]))
+	if (is_num(v[3]))
 		sim->time_to_compile = atoi(v[3]);
-	if (is_num(v[1]))
+	if (is_num(v[4]))
 		sim->time_to_debug = atoi(v[4]);
-	if (is_num(v[1]))
+	if (is_num(v[5]))
 		sim->time_to_refactor = atoi(v[5]);
-	if (is_num(v[1]))
+	if (is_num(v[6]))
 		sim->number_of_compiles_required = atoi(v[6]);
-	if (is_num(v[1]))
+	if (is_num(v[7]))
 		sim->dongle_cooldown = atoi(v[7]);
 	sim->scheduler = v[8];
 	if (valid_args(sim))
