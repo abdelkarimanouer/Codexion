1e4af0f0c417e9ce9fee04b6dfec1d4b491fff0e
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-04-18 06:04:04 +0100

    create fun to push the new request to the queue then put him in the right index

--- coders/init.c
+++ coders/init.c
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 15:35:20 by aanouer           #+#    #+#             */
-/*   Updated: 2026/04/17 15:40:22 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/04/18 05:45:53 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,10 @@ void	init_simulation_with_default_values(t_simulation **sim)
 	(*sim)->number_of_compiles_required = -1;
 	(*sim)->dongle_cooldown = -1;
 	(*sim)->scheduler = "";
+}
+
+void	init_queue_with_default_values(t_queue **queue, long n_of_coders)
+{
+	(*queue)->number_of_tickets = 0;
+	(*queue)->max_nums_of_tickets = n_of_coders;
 }
--- coders/queue.c
+++ coders/queue.c
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 18:11:24 by aanouer           #+#    #+#             */
-/*   Updated: 2026/04/17 18:15:37 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/04/18 06:03:25 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,27 @@ int	goes_first(t_request a, t_request b, char *scheduler)
 		}
 	}
 	return (0);
+}
+
+void	push_request(t_queue *queue, t_request new_request, char *scheduler)
+{
+	int			i;
+	int			the_winner;
+	t_request	temp;
+
+	i = queue->number_of_tickets;
+	queue->requests[i] = new_request;
+	queue->number_of_tickets++;
+	i = queue->number_of_tickets;
+	while (i > 0)
+	{
+		the_winner = (i - 1) / 2;
+		if (i > 1 && goes_first(queue->requests[i], queue->requests[the_winner], scheduler))
+		{
+			temp = queue->requests[i];
+			queue->requests[i] = queue->requests[the_winner];
+			queue->requests[the_winner] = temp;
+		}
+		i--;
+	}
 }
