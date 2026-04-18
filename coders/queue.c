ba47711277e96c69c917331d5da33f8cf5a245d8
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-04-18 06:06:53 +0100

    create fun to push the new request to the queue then put him in the right index

--- coders/queue.c
+++ coders/queue.c
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 18:11:24 by aanouer           #+#    #+#             */
-/*   Updated: 2026/04/18 06:03:25 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/04/18 06:06:10 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,11 +41,10 @@ void	push_request(t_queue *queue, t_request new_request, char *scheduler)
 	i = queue->number_of_tickets;
 	queue->requests[i] = new_request;
 	queue->number_of_tickets++;
-	i = queue->number_of_tickets;
 	while (i > 0)
 	{
 		the_winner = (i - 1) / 2;
-		if (i > 1 && goes_first(queue->requests[i], queue->requests[the_winner], scheduler))
+		if (goes_first(queue->requests[i], queue->requests[the_winner], scheduler))
 		{
 			temp = queue->requests[i];
 			queue->requests[i] = queue->requests[the_winner];
