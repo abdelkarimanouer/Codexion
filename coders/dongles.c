d2a81843a3a9423d37ba4761968f669e9412ce83
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-04-21 09:25:34 +0100

    update file

--- coders/dongles.c
+++ coders/dongles.c
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 09:12:17 by aanouer           #+#    #+#             */
-/*   Updated: 2026/04/21 09:21:50 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/04/21 09:24:34 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 void	take_dongle(t_coder *coder, t_dongle *dongle)
 {
-	t_request	*req;
+	t_request	req;
 
-	req = malloc(sizeof(t_request));
-	req->coder_id = coder->id;
+	req.coder_id = coder->id;
 	pthread_mutex_lock(&coder->sim->ticket_count_mutex);
-	req->ticket_number = coder->sim->ticket_count++;
+	req.ticket_number = coder->sim->ticket_count++;
 	pthread_mutex_unlock(&coder->sim->ticket_count_mutex);
 	pthread_mutex_lock(&coder->lock_l_c_s);
-	req->deadline = coder->last_compile_start + coder->sim->time_to_burnout;
+	req.deadline = coder->last_compile_start + coder->sim->time_to_burnout;
 	pthread_mutex_unlock(&coder->lock_l_c_s);
 	pthread_mutex_lock(&dongle->lock_dongle);
-	push_request(dongle->queue, *req, coder->sim->scheduler);
+	push_request(dongle->queue, req, coder->sim->scheduler);
 	while (dongle->is_available == 0
-		|| get_the_winner(dongle->queue)->coder_id != coder->id)
+		|| get_the_winner(dongle->queue)->coder_id != coder->id
+		|| get_current_time() < dongle->cooldown_end)
 		pthread_cond_wait(&dongle->condition, &dongle->lock_dongle);
 	dongle->is_available = 0;
 	pop_request(dongle->queue, coder->sim->scheduler);
