3f3afc5052fe796de2d7b6ac3ad197e422d8efd2
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-04-22 10:40:28 +0100

    update file

--- coders/threads.c
+++ coders/threads.c
@@ -6,11 +6,19 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 09:21:58 by aanouer           #+#    #+#             */
-/*   Updated: 2026/04/22 09:15:31 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/04/22 10:22:17 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
+
+static void	rest_of_code(t_coder *coder)
+{
+	print_action(coder->sim, coder->id, "is debugging");
+	my_sleep(coder->sim->time_to_debug, coder->sim);
+	print_action(coder->sim, coder->id, "is refactoring");
+	my_sleep(coder->sim->time_to_refactor, coder->sim);
+}
 
 void	*coder_routine(void *arg)
 {
@@ -23,6 +31,20 @@ void	*coder_routine(void *arg)
 	{
 		if (check_simulation_stop(coder->sim))
 			break ;
+		take_dongle(coder, coder->left_dongle);
+		take_dongle(coder, coder->right_dongle);
+		pthread_mutex_lock(&coder->lock_l_c_s);
+		coder->last_compile_start = get_current_time();
+		pthread_mutex_unlock(&coder->lock_l_c_s);
+		print_action(coder->sim, coder->id, "is compiling");
+		my_sleep(coder->sim->time_to_compile, coder->sim);
+		release_dongle(coder, coder->right_dongle);
+		release_dongle(coder, coder->left_dongle);
+		coder->compile_count++;
+		if (coder->compile_count >= coder->sim->number_of_compiles_required)
+			break;
+		else
+			rest_of_code(coder);
 	}
 	return (NULL);
 }
