bd3674de0d48d321be18d103916f7a5ac0b6138a
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-05-02 09:39:02 +0100

    update file

--- coders/threads.c
+++ coders/threads.c
@@ -6,7 +6,7 @@
 /*   By: aanouer <aanouer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 09:21:58 by aanouer           #+#    #+#             */
-/*   Updated: 2026/04/29 10:03:10 by aanouer          ###   ########.fr       */
+/*   Updated: 2026/05/02 09:38:44 by aanouer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,35 +41,30 @@ static void	handle_coder_thread_fail(t_simulation *sim, long i)
 		pthread_join(sim->coders[i].coder_thread, NULL);
 }
 
-int	start_threads(t_simulation *sim)
+static int	handle_monitor_fail(t_simulation *sim)
 {
 	long	i;
 
+	pthread_mutex_lock(&sim->sync_mutex);
+	sim->threads_ready = -1;
+	pthread_cond_broadcast(&sim->sync_cond);
+	pthread_mutex_unlock(&sim->sync_mutex);
+	pthread_mutex_lock(&sim->stop_mutex);
+	sim->stop = 1;
+	pthread_mutex_unlock(&sim->stop_mutex);
 	i = 0;
 	while (i < sim->number_of_coders)
 	{
-		if (pthread_create(&sim->coders[i].coder_thread, NULL, coder_routine,
-				&sim->coders[i]) != 0)
-			return (handle_coder_thread_fail(sim, i), 1);
+		pthread_join(sim->coders[i].coder_thread, NULL);
 		i++;
 	}
-	if (pthread_create(&sim->monitor, NULL, monitor_routine, sim) != 0)
-	{
-		pthread_mutex_lock(&sim->sync_mutex);
-		sim->threads_ready = -1;
-		pthread_cond_broadcast(&sim->sync_cond);
-		pthread_mutex_unlock(&sim->sync_mutex);
-		pthread_mutex_lock(&sim->stop_mutex);
-		sim->stop = 1;
-		pthread_mutex_unlock(&sim->stop_mutex);
-		i = 0;
-		while (i < sim->number_of_coders)
-		{
-			pthread_join(sim->coders[i].coder_thread, NULL);
-			i++;
-		}
-		return (1);
-	}
+	return (1);
+}
+
+static void	start_simulation(t_simulation *sim)
+{
+	long	i;
+
 	pthread_mutex_lock(&sim->sync_mutex);
 	sim->threads_ready = 1;
 	sim->start_timestamp = get_current_time();
@@ -89,5 +84,22 @@ int	start_threads(t_simulation *sim)
 	pthread_mutex_unlock(&sim->stop_mutex);
 	pthread_cond_broadcast(&sim->sync_cond);
 	pthread_mutex_unlock(&sim->sync_mutex);
+}
+
+int	start_threads(t_simulation *sim)
+{
+	long	i;
+
+	i = 0;
+	while (i < sim->number_of_coders)
+	{
+		if (pthread_create(&sim->coders[i].coder_thread, NULL, coder_routine,
+				&sim->coders[i]) != 0)
+			return (handle_coder_thread_fail(sim, i), 1);
+		i++;
+	}
+	if (pthread_create(&sim->monitor, NULL, monitor_routine, sim) != 0)
+		return (handle_monitor_fail(sim));
+	start_simulation(sim);
 	return (0);
 }
